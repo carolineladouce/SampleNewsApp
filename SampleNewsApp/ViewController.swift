65d19e7adfe72bd239eee75b20d9045eb226fe1f
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-08-06 22:11:16 -0400

    Initial Commit

---
+++ SampleNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleNewsApp
+//
+//  Created by Caroline LaDouce on 8/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
