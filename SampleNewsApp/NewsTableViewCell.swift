078a706f72c3c095d2887d817fad4d908e1a1b06
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-08-09 21:53:40 -0400

    NewsTableView cell's now display the title, subtitle, and image view

--- SampleNewsApp/NewsTableViewCell.swift
+++ SampleNewsApp/NewsTableViewCell.swift
@@ -31,19 +31,22 @@ class NewsTableViewCell: UITableViewCell {
     
     private let newsTitleLabel: UILabel = {
         let label = UILabel()
+        label.numberOfLines = 0
         label.font = .systemFont(ofSize: 25, weight: .medium)
         return label
     }()
     
-    private let subtitleLabel: UILabel = {
+    private let newsSubtitleLabel: UILabel = {
         let label = UILabel()
+        label.numberOfLines = 0
         label.font = .systemFont(ofSize: 18, weight: .regular)
         return label
     }()
     
     private let newsImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = .systemRed
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .secondarySystemBackground
         imageView.contentMode = .scaleAspectFill
         return imageView
     }()
@@ -52,7 +55,7 @@ class NewsTableViewCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         contentView.addSubview(newsTitleLabel)
-        contentView.addSubview(subtitleLabel)
+        contentView.addSubview(newsSubtitleLabel)
         contentView.addSubview(newsImageView)
     }
     
@@ -65,23 +68,45 @@ class NewsTableViewCell: UITableViewCell {
         
         newsTitleLabel.frame = CGRect(x: 10,
                                       y: 0,
-                                      width: contentView.frame.size.width - 120,
-                                      height: contentView.frame.size.height / 2)
+                                      width: contentView.frame.size.width - 170,
+                                      height: 70)
+        
+        newsSubtitleLabel.frame = CGRect(x: 10,
+                                         y: 70,
+                                         width: contentView.frame.size.width - 170,
+                                         height: contentView.frame.size.height / 2)
+        
+        newsImageView.frame = CGRect(x: contentView.frame.size.width - 160,
+                                         y: 5,
+                                         width: 150,
+                                         height: contentView.frame.size.height - 10)
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        newsTitleLabel.text = nil
+        newsSubtitleLabel.text = nil
+        newsImageView.image = nil 
     }
     
     func configure(with viewModel: NewsTableViewCellViewModel) {
         newsTitleLabel.text = viewModel.title
-        subtitleLabel.text = viewModel.subtitle
+        newsSubtitleLabel.text = viewModel.subtitle
         
         // Image
         if let data = viewModel.imageData {
             newsImageView.image = UIImage(data: data)
-        } else {
+        } else if let url = viewModel.imageURL {
             // Fetch
+            URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+                guard let data = data, error == nil else {
+                    return
+                }
+                viewModel.imageData = data
+                DispatchQueue.main.async {
+                    self?.newsImageView.image = UIImage(data: data)
+                }
+            }.resume()
         }
         
     }
